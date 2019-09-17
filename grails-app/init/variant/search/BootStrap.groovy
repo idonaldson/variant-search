69120c3cd19645d3d33e6ef4125d15eb02cbfb8f
Ian Donaldson <ian.donaldson@ndsu.edu>
2019-09-17 09:54:04 -0500

    Initial parse of TSV file into H2 database when starting up

--- grails-app/init/variant/search/BootStrap.groovy
+++ grails-app/init/variant/search/BootStrap.groovy
@@ -1,8 +1,27 @@
 package variant.search
 
+import org.grails.io.support.ClassPathResource
+import static com.xlson.groovycsv.CsvParser.parseCsv
+
+
 class BootStrap {
 
+    def grailsApplication
+
     def init = { servletContext ->
+        if (Gene.count() == 0) {
+            def filePath = "resources/variants.tsv"
+            def text = grailsApplication.getParentContext().getResource("classpath:$filePath").getInputStream().getText()
+
+            def data = parseCsv(text, separator: '\t')
+
+            for (geneData in data) {
+                if (geneData["Gene"].toString().length() > 0){
+                    def gene = new Gene(name: geneData["Gene"]).save(failOnError: true);
+                }
+            }
+        }
+
     }
     def destroy = {
     }
